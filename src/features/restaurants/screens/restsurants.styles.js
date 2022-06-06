c12d6eebfd05d73d17020dc75dea1f74e441648d
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-06-06 18:47:18 +0300

    Implemented navigation

--- src/features/restaurants/screens/restsurants.styles.js
+++ src/features/restaurants/screens/restsurants.styles.js
@@ -1,11 +1,6 @@
 import styled from "styled-components/native";
 import { FlatList, StatusBar } from "react-native";
 
-export const RestaurantsScreenContainer = styled.SafeAreaView`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
 export const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
