b6faf54718d8b0a689b841fd23938bdaf51da874
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-06-07 10:07:28 +0300

    Created server with mocked data

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,22 +1,25 @@
+import { mocks } from "./mock";
 import camelize from "camelize";
 
-export const restaurantsRequest = (location) => {
-  return fetch(
-    `http://localhost:5001/mealstogo-b2612/us-central1/placesNearby?location=${location}`
-  ).then((res) => {
-    return res.json();
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
   });
 };
 
-export const restaurantsTransform = ({ results = [] }) => {
-  const mappedResults = results.map((restaurant) => {
-    return {
-      ...restaurant,
-      address: restaurant.vicinity,
-      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
-    };
-  });
-
-  return camelize(mappedResults);
+const restaurantsTransform = (result) => {
+  return camelize(result);
 };
+
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
