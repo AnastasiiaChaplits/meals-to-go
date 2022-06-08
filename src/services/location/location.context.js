e5de303573afd2cfd35bd5cec1e1b5295645c2e0
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-06-08 13:45:19 +0300

    Improved search

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { locationRequest, locationTransform } from "./location.service";
 
 export const LocationContext = createContext();
@@ -12,10 +12,13 @@ export const LocationContextProvider = ({ children }) => {
   const onSearch = (searchKeyword) => {
     setLocation(true);
     setKeyword(searchKeyword);
-    if (!searchKeyword.length) {
+  };
+
+  useEffect(() => {
+    if (!keyword.length) {
       return;
     }
-    locationRequest(searchKeyword.toLowerCase())
+    locationRequest(keyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
@@ -25,7 +28,8 @@ export const LocationContextProvider = ({ children }) => {
         setIsLoading(false);
         setError(error);
       });
-  };
+  }, [keyword]);
+
   return (
     <LocationContext.Provider
       value={{
