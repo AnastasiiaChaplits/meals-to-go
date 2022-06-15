c79b59fb5ee3ab7fd9532dfdc85fd7ad349ef54c
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-06-15 11:23:39 +0300

    Stored favourites list between session

--- src/components/restaurant/compact-restaurant-info.component.js
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -10,8 +10,8 @@ import {
 
 const isAndroid = Platform.OS === "android";
 
-export const CompactRestaurantInfo = ({ restaurant }) => {
-  const Image = isAndroid ? CompactVebview : CompactImage;
+export const CompactRestaurantInfo = ({ restaurant, isMap }) => {
+  const Image = isAndroid && isMap ? CompactVebview : CompactImage;
   return (
     <Item>
       <Image source={{ uri: restaurant.photos[0] }} />
