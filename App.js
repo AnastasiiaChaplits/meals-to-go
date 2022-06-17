29766296e935465756c22b90cb047f8ccfc6e743
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-06-17 13:25:59 +0300

    Set up firebase and created authentication context provide

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
@@ -13,6 +15,20 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBE0zxTqvNIyI0olFUWR9C7eczZpd6Yj8E",
+  authDomain: "mealstogo-4c072.firebaseapp.com",
+  projectId: "mealstogo-4c072",
+  storageBucket: "mealstogo-4c072.appspot.com",
+  messagingSenderId: "1005842547876",
+  appId: "1:1005842547876:web:4ff198a8e6ed99d1d29231",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   let [oswaldLoaded] = useOswald({
@@ -29,13 +45,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,6 +1,5 @@
 import React, { useState, createContext } from "react";
 import * as firebase from "firebase";
-
 import { loginRequest } from "./authentication.service";
 
 export const AuthenticationContext = createContext();
@@ -9,15 +8,6 @@ export const AuthenticationContextProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
-
-  firebase.auth().onAuthStateChanged((usr) => {
-    if (usr) {
-      setUser(usr);
-      setIsLoading(false);
-    } else {
-      setIsLoading(false);
-    }
-  });
 
   const onLogin = (email, password) => {
     setIsLoading(true);
@@ -28,49 +18,16 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e.toString());
+        setError(e);
       });
   };
-
-  const onRegister = (email, password, repeatedPassword) => {
-    setIsLoading(true);
-    if (password !== repeatedPassword) {
-      setError("Error: Passwords do not match");
-      return;
-    }
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((u) => {
-        setUser(u);
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setIsLoading(false);
-        setError(e.toString());
-      });
-  };
-
-  const onLogout = () => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        setUser(null);
-        setError(null);
-      });
-  };
-
   return (
     <AuthenticationContext.Provider
       value={{
-        isAuthenticated: !!user,
         user,
         isLoading,
         error,
         onLogin,
-        onRegister,
-        onLogout,
       }}
     >
       {children}
