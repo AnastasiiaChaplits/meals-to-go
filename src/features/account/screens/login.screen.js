27f65c69c3aea6b3f550a026b378696cd7cb9547
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-07-01 14:41:18 +0300

    Created login screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
-import { AccountBackground } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  AuthInput,
+} from "../components/account.styles";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "react-native-paper";
 
 export const LoginScreen = () => {
-  return <AccountBackground />;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { onLogin, error } = useContext(AuthenticationContext);
+
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <AccountContainer>
+        <AuthInput
+          label="E-mail"
+          value={email}
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          onChangeText={(u) => setEmail(u)}
+        />
+        <AuthInput
+          label="Password"
+          value={password}
+          textContentType="password"
+          secureTextEntry
+          autoCapitalize="none"
+          secure
+          onChangeText={(p) => setPassword(p)}
+        />
+        {error && (
+          <Spacer size="large">
+            <Text variant="error">{error}</Text>
+          </Spacer>
+        )}
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => onLogin(email, password)}
+          >
+            Login
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
 };
