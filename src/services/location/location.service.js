4f198be3093c67694f9e28ac868091e54115ae77
AnastasiiaChaplits <anastasia.chaplits@gmail.com>
2022-07-25 15:55:15 +0300

    Move locations mock to server

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,20 +1,16 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `http://localhost:5001/mealstogo-4c072/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
   const formattedResponse = camelize(result);
-  const { geometry = {} } = camelize(formattedResponse.results)[0];
+  const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
 
   return { lat, lng, viewport: geometry.viewport };
